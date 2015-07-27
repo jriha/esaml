4880e3a573790a8178013b4134beace1ef4fe250
Josua Riha <josua.riha@secucloud.com>
2015-07-27 10:07:43 +0200

    Update.

--- src/xmerl_dsig.erl
+++ src/xmerl_dsig.erl
@@ -38,9 +38,14 @@ strip(#xmlDocument{content = Kids} = Doc) ->
 
 strip(#xmlElement{content = Kids} = Elem) ->
     NewKids = lists:filter(fun(Kid) ->
-        case xmerl_c14n:canon_name(Kid) of
-            "http://www.w3.org/2000/09/xmldsig#Signature" -> false;
-            _Name -> true
+        case Kid of
+            #xmlText{} ->
+                true;
+            _ ->
+                case xmerl_c14n:canon_name(Kid) of
+                    "http://www.w3.org/2000/09/xmldsig#Signature" -> false;
+                    _Name -> true
+                end
         end
     end, Kids),
     Elem#xmlElement{content = NewKids}.
